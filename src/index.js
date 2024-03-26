f2532f775025338ffa0f1d0ac31c5b3a3c17ace0
Z K <zee.kazmi@icloud.com>
2024-03-26 19:12:47 +0000

    Updated organization of file, linked rows to pug view to render in.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 /* Import dependencies */
 const express = require("express");
 const mysql = require("mysql2");
+const path = require("path");
 
 /* Create express instance */
 const app = express();
 const port = 3000;
+
+/* Load view engine to render pug files */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+/* */
 
 /* Setup database connection */
 const db = mysql.createConnection({
@@ -16,7 +23,7 @@ const db = mysql.createConnection({
 
 /* Landing route */
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.render('home');
 });
 
 // Sample API route
@@ -24,11 +31,15 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
+app.get("/city", (req, res) => {
+  res.render('city');
+});
+
 // Returns an array of cities from the database
 app.get("/cities", (req, res) => {
   db.execute("SELECT * FROM `city`", (err, rows, fields) => {
     console.log(`/cities: ${rows.length} rows`);
-    return res.send(rows);
+    return res.render('cities', {rows:rows});
   });
 });
 
