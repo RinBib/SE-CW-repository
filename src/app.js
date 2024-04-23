8d06d69f939fac90aa51518976d91bf3dd7c0941
Z K <zee.kazmi@icloud.com>
2024-04-23 16:12:51 +0100

    app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// Register
+app.get('/register', function (req, res) {
+    res.render('register');
+   });
+   // Login
+   app.get('/login', function (req, res) {
+    res.render('login');
+   });
