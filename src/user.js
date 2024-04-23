97ad5ede0747c8c4dbf373276ce4c1a34359fd64
Z K <zee.kazmi@icloud.com>
2024-04-23 16:28:22 +0100

    now tests if entered email exists in db

--- src/user.js
+++ src/user.js
@@ -30,3 +30,18 @@ class User {
 module.exports = {
  User
 }
+
+// check if entered email exists in the database
+async getIdFromEmail() 
+{
+    var sql = "SELECT id FROM Users WHERE Users.email = ?";
+    const result = await db.query(sql, [this.email]);
+    // TODO LOTS OF ERROR CHECKS HERE..
+    if (JSON.stringify(result) != '[]') {
+        this.id = result[0].id;
+        return this.id;
+    }
+    else {
+        return false;
+    }
+}
